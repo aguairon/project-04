9279b0d7b0fc26034a6190895d044d757a663ed2
Begona Fernandez <boadicea1979@yahoo.es>
2019-02-27 14:08:23 +0000

    added route for updating and deleting articles

--- controllers/articles.py
+++ controllers/articles.py
@@ -1,4 +1,5 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify, g
+from lib.secure_route import secure_route
 from models.article import Article, ArticleSchema
 
 api = Blueprint('articles', __name__)
@@ -15,3 +16,47 @@ def index():
 def show(article_id):
     article = Article.query.get(article_id)
     return article_schema.jsonify(article)
+
+@api.route('/articles', methods=['POST'])
+@secure_route
+def create():
+    article, errors = article_schema.load(request.get_json())
+    article.creator = g.current_user
+
+    if errors:
+        return jsonify(errors), 422
+
+    article.save()
+
+    return article_schema.jsonify(article)
+
+@api.route('/articles/<int:article_id>', methods=['PUT'])
+@secure_route
+def update(article_id):
+    article = Article.query.get(article_id)
+
+    if article.creator != g.current_user:
+        return jsonify({'message':'Unauthorized'}), 401
+    article, errors = article_schema.load(request.get_json(), instance=article)
+
+    if errors:
+        return jsonify(errors), 422
+
+    article.save()
+
+    return article_schema.jsonify(article)
+
+@api.route('/articles/<int:article_id>', methods=['DELETE'])
+@secure_route
+def delete(article_id):
+    article = Article.query.get(article_id)
+
+    if not article:
+        return jsonify({'message': 'Not found'}), 404
+    if  article.creator != g.current_user:
+        return jsonify({'message':'Unauthorized'}), 401
+
+
+    article.remove()
+
+    return '', 204
--- lib/secure_route.py
+++ lib/secure_route.py
@@ -4,19 +4,18 @@ from flask import request, jsonify, g
 from models.user import User
 from config.environment import secret
 
-
 def secure_route(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
         if 'Authorization' not in request.headers:
             return jsonify({'message': 'Unauthorized'}), 401
 
-        token = request.headers.get('Authorization').replace('Bearer', '')
+        token = request.headers.get('Authorization').replace('Bearer ', '')
         payload = jwt.decode(token, secret)
         user = User.query.get(payload.get('sub'))
 
         if not user:
-            return jsonify({'message' : 'Unauthorized'}), 401
+            return jsonify({'message': 'Unauthorized'}), 401
 
         g.current_user = user
 
